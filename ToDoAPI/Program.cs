ab3783eb317d252053f8a89ab5ce4972fb333e14
DMeier12 <runeloz2294@gmail.com>
2023-03-16 20:48:42 -0500

    Step 4

--- ToDoAPI/Program.cs
+++ ToDoAPI/Program.cs
@@ -8,6 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ToDoAPI.Models.ToDoContext>(
+    options =>
+    {
+        //The string in the parameter for GetConnectionString() should match the name in appsettings.json
+        options.UseSqlServer(builder.Configuration.GetConnectionString("TodoDB"));
+    }
+    );
 
 var app = builder.Build();
 
