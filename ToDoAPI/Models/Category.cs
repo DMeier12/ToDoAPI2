6fe83036370c16de23a07005383360174a258a0b
DMeier12 <runeloz2294@gmail.com>
2023-03-16 20:44:08 -0500

    Add project files.

---
+++ ToDoAPI/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToDoAPI.Models;
+
+public partial class Category
+{
+    public int CategoryId { get; set; }
+
+    public string CatName { get; set; } = null!;
+
+    public string? CatDesc { get; set; }
+
+    public virtual ICollection<ToDo> ToDos { get; } = new List<ToDo>();
+}
---
+++ ToDoAPI/Models/ToDoContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoAPI.Models;
+
+public partial class ToDoContext : DbContext
+{
+    public ToDoContext()
+    {
+    }
+
+    public ToDoContext(DbContextOptions<ToDoContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<ToDo> ToDos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=ToDoDB");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.Property(e => e.CategoryId).ValueGeneratedNever();
+            entity.Property(e => e.CatDesc).HasMaxLength(100);
+            entity.Property(e => e.CatName).HasMaxLength(25);
+        });
+
+        modelBuilder.Entity<ToDo>(entity =>
+        {
+            entity.Property(e => e.ToDoId).ValueGeneratedNever();
+            entity.Property(e => e.Name).HasMaxLength(100);
+
+            entity.HasOne(d => d.Category).WithMany(p => p.ToDos)
+                .HasForeignKey(d => d.CategoryId)
+                .HasConstraintName("FK_ToDos_Categories");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
